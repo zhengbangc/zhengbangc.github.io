70638501eb0b55fa7c468896933652276d187d55
zhengbangc <zhengbangc@gmail.com>
2016-05-26 23:12:57 -0500

    update gh-pages

---
+++ js/app.js
@@ -0,0 +1,1 @@
+var app=angular.module("mp4",["ngRoute","mp4Controllers","mp4Services"]);app.config(["$routeProvider",function($routeProvider){$routeProvider.when("/home",{templateUrl:"partials/home.html",controller:"homeController"}).otherwise({redirectTo:"/home"})}]);
